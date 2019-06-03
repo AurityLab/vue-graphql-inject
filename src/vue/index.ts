742d0031460ce74b05a62a6ca11c7d0cc8d6353b
Andre Loreth <andre.loreth@live.de>
2019-06-03 18:59:11 +0200

    fix(vue): Adjust definition interface

--- src/vue/index.ts
+++ src/vue/index.ts
@@ -1,12 +1,16 @@
 import _Vue from 'vue'
 import {DocumentNode} from "graphql";
 
+interface DefinitionInterface {
+    queries: { [key: string]: DocumentNode };
+    mutations: { [key: string]: DocumentNode };
+    subscriptions: { [key: string]: DocumentNode };
+}
+
 export interface VueGraphQLInjectOptions {
     definition: { [key: string]: DocumentNode };
 }
 
-export function VueGraphQLInject(Vue: typeof _Vue, options: VueGraphQLInjectOptions): void{
-    Vue.prototype.$gql = (): { operations: { [key: string]: DocumentNode } } => ({
-        operations: options.definition
-    })
+export function VueGraphQLInject(Vue: typeof _Vue, options: VueGraphQLInjectOptions): void {
+    Vue.prototype.$gql = options.definition
 }
